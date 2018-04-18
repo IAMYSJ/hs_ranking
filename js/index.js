92b47a90ed0fdf0822d9cf778e31391bca39f5e6
YSJ <s1003352@gmail.com>
2018-04-18 22:52:34 +0800

    加入圖片

--- js/index.js
+++ js/index.js
@@ -1,13 +1,165 @@
+let cardDict = {
+    0: 'arcane-keysmith-hd.png',
+    1: 'archmage-arugal-hd.png',
+    2: 'azalina-soulthief-hd.png',
+    3: 'baku-the-mooneater-hd.png',
+    4: 'baleful-banker.png',
+    5: 'bellringer-sentry-hd.png',
+    6: 'bewitched-guardian-hd.png',
+    7: 'black-cat-hd.png',
+    8: 'blackhowl-gunspire-hd.png',
+    9: 'blackwald-pixie-hd.png',
+    10: 'blazing-invocation.png',
+    11: 'blink-fox-hd.png',
+    12: 'blood-witch.png',
+    13: 'bogshaper.png',
+    14: 'bonfire-elemental-hd.png',
+    15: 'book-of-specters-hd.png',
+    16: 'carrion-drake.png',
+    17: 'cathedral-gargoyle.png',
+    18: 'cauldron-elemental.png',
+    19: 'chameleos-hd.png',
+    20: 'cheap-shot-hd.png',
+    21: 'chief-inspector.png',
+    22: 'cinderstorm.png',
+    23: 'clockwork-automaton-hd.png',
+    24: 'coffin-crasher-hd.png',
+    25: 'countess-ashmore-hd.png',
+    26: 'curio-collector.png',
+    27: 'curse-of-weakness.png',
+    28: 'cursed-castaway.png',
+    29: 'cutthroat-buccaneer.png',
+    30: 'darius-crowley-hd.png',
+    31: 'Dark-Possession.png',
+    32: 'darkmire-moonkin.png',
+    33: 'deadly-arsenal-hd.png',
+    34: 'deathweb-spider.png',
+    35: 'deranged-doctor.png',
+    36: 'dire-frenzy.png',
+    37: 'divine-hymn.png',
+    38: 'dollmaster-dorian-hd.png',
+    39: 'druid-of-the-scythe-hd.png',
+    40: 'duskbat.png',
+    41: 'duskfallen-aviana-hd.png',
+    42: 'duskhaven-hunter-hd.png',
+    43: 'earthen-might.png',
+    44: 'emeriss-hd.png',
+    45: 'face-collector.png',
+    46: 'felsoul-inquisitor.png',
+    47: 'ferocious-howl.png',
+    48: 'festeroot-hulk-hd-1.png',
+    49: 'fiendish-circle.png',
+    50: 'forest-guide-hd.png',
+    51: 'Furious-Ettin.png',
+    52: 'genn-greymane-hd.png',
+    53: 'ghost-light-angler.png',
+    54: 'ghostly-charger.png',
+    55: 'gilnean-royal-guard-hd.png',
+    56: 'glinda-crowskin-hd.png',
+    57: 'glitter-moth-hd.png',
+    58: 'gloom-stag-hd.png',
+    59: 'hagatha-the-witch-hd.png',
+    60: 'hench-clan-thug.png',
+    61: 'hidden-wisdom.png',
+    62: 'holy-water-hd.png',
+    63: 'houndmaster-shaw-hd.png',
+    64: 'hunting-mastiff-1.png',
+    65: 'lady-in-white-hd.png',
+    66: 'lifedrinker.png',
+    67: 'lord-godfrey-hd.png',
+    68: 'lost-spirit.png',
+    69: 'mad-hatter.png',
+    70: 'marsh-drake.png',
+    71: 'militia-commander-hd.png',
+    72: 'mistwraith-hd.png',
+    73: 'mossy-horror-hd.png',
+    74: 'muck-hunter-hd.png',
+    75: 'murkspark-eel-hd.png',
+    76: 'night-prowler.png',
+    77: 'nightmare-amalgam-hd.png',
+    78: 'nightscale-matriarch.png',
+    79: 'paragon-of-light-hd.png',
+    80: 'phantom-militia-hd.png',
+    81: 'pick-pocket-hd.png',
+    82: 'prince-liam.png',
+    83: 'pumpkin-peasant-hd.png',
+    84: 'quartz-elemental.png',
+    85: 'rabid-worgen.png',
+    86: 'rat-trap-hd.png',
+    87: 'ratcatcher.png',
+    88: 'ravencaller.png',
+    89: 'rebuke-hd.png',
+    90: 'redband-wasp-hd.png',
+    91: 'rotten-applebaum-hd.png',
+    92: 'Sandbinder.png',
+    93: 'scaleworm-hd.png',
+    94: 'shudderwock.png',
+    95: 'silver-sword.png',
+    96: 'snap-freeze.png',
+    97: 'sound-the-bells-hd.png',
+    98: 'spectral-cutlass-hd.png',
+    99: 'spellshifter-hd.png',
+    100: 'splintergraft-hd.png',
+    101: 'splitting-festeroot-hd.png',
+    102: 'squashling.png',
+    103: 'swamp-dragon-egg.png',
+    104: 'swamp-leech.png',
+    105: 'swift-messenger-hd.png',
+    106: 'tanglefur-mystic.png',
+    107: 'tess-greymane-hd.png',
+    108: 'the-glass-knight-hd.png',
+    109: 'toki-time-tinker-hd.png',
+    110: 'totem-cruncher-hd.png',
+    111: 'town-crier-hd.png',
+    112: 'toxmonger-hd.png',
+    113: 'unpowered-steambot.png',
+    114: 'vex-crow-hd.png',
+    115: 'vicious-scalehide.png',
+    116: 'vilebrood-skitterer.png',
+    117: 'vivid-nightmare-hd.png',
+    118: 'voodoo-doll-hd.png',
+    119: 'walnut-sprite.png',
+    120: 'wanted-hd.png',
+    121: 'warpath.png',
+    122: 'wing-blast-hd.png',
+    123: 'wispering-woods-hd.png',
+    124: 'witching-hour-hd.png',
+    125: 'witchs-apprentice-hd.png',
+    126: 'witchs-cauldron-hd.png',
+    127: 'witchwood-apple-hd.png',
+    128: 'witchwood-grizzly-hd.png',
+    129: 'Witchwood-Imp.png',
+    130: 'witchwood-piper-hd.png',
+    131: 'woodcutters-axe-hd.png',
+    132: 'worgen-abomination-hd.png',
+    133: 'wyrmguard.png',
+    134: 'zap.png'
+}
+
+randomNum = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 
+    51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 
+    61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 
+    71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 
+    81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 
+    91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 
+    101, 102, 103, 104, 105, 106, 107, 108, 
+    109, 110, 111, 112, 113, 114, 115, 116, 
+    117, 118, 119, 120, 121, 122, 123, 124, 
+    125, 126, 127, 128, 129, 130, 131, 132, 
+    133, 134
+]
+
 window.addEventListener('load', function() {
-    card = document.getElementById('card');
-	card.addEventListener('dragend', function() {
-        setTimeout(function() {
-            card.style.visibility = 'visible';
-        }, 500);
-    }, false);
+
+    const card = document.getElementById('card');
     
     // Drop Left
-    leftbox = document.getElementById('leftbox');
+    const leftbox = document.getElementById('leftbox');
 	leftbox.addEventListener('dragover', function(e) {
         e.preventDefault();
         leftbox.style.background = 'lightblue';
@@ -17,13 +169,19 @@ window.addEventListener('load', function() {
         leftbox.style.background = 'white';
     }, false);
     leftbox.addEventListener('drop', function(e) {
-        //e.preventDefault();
         card.style.visibility = 'hidden';
+        const randomNum = Math.floor((Math.random() * 134));
+        card.src = 'image/' + cardDict[randomNum];
+        delete cardDict[randomNum];
+        console.log(randomNum);
+        setTimeout(function() {
+            card.style.visibility = 'visible';          
+        }, 500);
         leftbox.style.background = 'white';
     }, false);
     
     // Drop Right
-    rightbox = document.getElementById('rightbox');
+    const rightbox = document.getElementById('rightbox');
 	rightbox.addEventListener('dragover', function(e) {
         e.preventDefault();
         rightbox.style.background = 'lightblue';
@@ -33,8 +191,14 @@ window.addEventListener('load', function() {
         rightbox.style.background = 'white';
     }, false);
 	rightbox.addEventListener('drop', function(e) {
-        //e.preventDefault();
         card.style.visibility = 'hidden';
+        const randomNum = Math.floor((Math.random() * 134));
+        card.src = 'image/' + cardDict[randomNum];
+        delete cardDict[randomNum];
+        console.log(randomNum);
+        setTimeout(function() {
+            card.style.visibility = 'visible';          
+        }, 500);
         rightbox.style.background = 'white';
     }, false);
 }, false);
